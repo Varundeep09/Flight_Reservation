1bd8ec8c9eb9a25fae8ad2a0cd9f9a20dab76fa0
Varundeep09 <varun123sengar@gmail.com>
2025-05-24 00:56:49 +0530

    Create Booking.java

---
+++ src/main/java/com/airline/model/Booking.java
@@ -0,0 +1,35 @@
+package com.airline.model;
+
+public class Booking {
+	private int id;
+	private int  userId;
+	private int flightId;
+	private String bookingDate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getFlightId() {
+		return flightId;
+	}
+	public void setFlightId(int flightId) {
+		this.flightId = flightId;
+	}
+	public String getBookingDate() {
+		return bookingDate;
+	}
+	public void setBookingDate(String bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+	
+	
+
+}
